c00fc5b5bce4a42b44c5be623324ece544f2076f
Aidan Waterman <watermana@msoe.edu>
2021-11-10 15:37:07 -0600

    lab finished

--- Lab9/rsa.py
+++ Lab9/rsa.py
@@ -10,32 +10,53 @@
 
 Introduction: (Describe the lab in your own words)
 
-In this lab we wrote functions that made public/private key pairs, encrypted a message using the public key, and calculated the private key when only given the public key.
+In this lab we wrote functions that made public/private key pairs, encrypted a message using the public key,
+and calculated the private key when only given the public key.
 
 
 Question 1: RSA Security
 In this lab, Trudy is able to find the private key from the public key. Why is this not a problem for RSA in practice?
 
-This is not a problem in practice because RSA is designed to take long enough to crack that by the time Trudy would be able to decode the message, the information in the message is no longer valuable.
+This is not a problem in practice because RSA is designed to take long enough to crack that by the time Trudy would
+be able to decode the message, the information in the message is no longer valuable.
 
 
 
 Question 2: Critical Step(s)
-When creating a key, Bob follows certain steps. Trudy follows other steps to break a key. What is the difference between Bob’s steps and Trudy’s so that Bob is able to run his steps on large numbers, but Trudy cannot run her steps on large numbers?
+When creating a key, Bob follows certain steps. Trudy follows other steps to break a key. What is the
+difference between Bob’s steps and Trudy’s so that Bob is able to run his steps on large numbers,
+but Trudy cannot run her steps on large numbers?
 
-Bob's steps have many possible configurations, this is because the requirements to make a public/private key pair are not extremely strict. Trudy's steps are not significantly more complex than Bob's from a mathmatical standpoint, but there is only one possible solution so this takes significantly longer to calculate.
+Bob's steps have many possible configurations, this is because the requirements to make a public/private key pair
+are not extremely strict. Trudy's steps are not significantly more complex than Bob's from a mathmatical standpoint,
+but there is only one possible solution so this takes significantly longer to calculate.
 
 
 Checksum Activity:
 Provide a dicussion of your experiences as described in the activity.  Be sure to answer all questions.
 
+1)
+    Public Key: (17, 10481)
+    Private Key: (4205, 10481)
+    Checksum: 213E
 
+2)
+    Forged Message: Bob owes Trudy $901.09
 
+3)
+    Yes
+
+4)
+    If the message is encoded with Alice's private key before it is sent to Bob then by decoding it using
+    Alice's public key, it can be confirmed that she is the one who sent it and it was not tampered by Trudy.
 
 
 Summary: (Summarize your experience with the lab, what you learned, what you liked,what you disliked, and any suggestions you have for improvement)
 
-
+This lab was fairly simple once the activity was completed, the hardest part was definitely when we were simulating alice and bob in the activity,
+during this stage we got a little confused and had to repeatedly try to recompute numbers because we made mistakes the first time around.
+The best part of this lab was learning how to break RSA, this feels like the most useful real world application to most of the
+material we have learned so far.
 
 
 
